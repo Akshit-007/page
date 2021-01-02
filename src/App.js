ba67da9daf43c08e7d034c3c703624c50302e454
Akshit <akshittrivedi1005@gmail.com>
2021-01-02 13:53:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="container">
+        <div className="Header"></div>
+        <div className="photo"></div>
+        <div className="stext">
+        <b>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</b>
+
+<ul>
+<li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+<li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. </li>
+</ul>
+        </div>
+        <div className="award"></div>
+        <div className="line">INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION. </div>
+        <div className="allphotos"></div>
+        <div className="info">Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors <br /> <hr/>      </div>
+                
+        <div className="product"><b>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</b>
+        <ul className="list">
+<li> &nbsp; CHEMICALS    </li>
+<li> &nbsp;PROCESS POWER WATER    </li>
+<li> &nbsp;WASTE WATER OILS    </li>
+<li> &nbsp;GAS PHARMA SUGARS    </li>
+<li> &nbsp;DISTILLERIES PAPER    </li>
+<li> &nbsp;PULP MARINE    </li>
+<li> &nbsp;DEFENCE METAL    </li>
+<li> &nbsp;MINING FOOD    </li>
+<li> &nbsp;BEVERAGE PETROCHEMICAL    </li>
+<li> &nbsp;REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE    </li>
+<li> &nbsp;RESIDENTIAL    </li>
+
+</ul>
+        </div>
+        <div className="Footer">
+        <div className="contact">
+
+<div className="i1" ><i class='fas fa-phone-alt'></i>&nbsp; Toll Free:1800 200 1234</div>
+<div className="i2"><i class="fab fa-facebook-f"></i> &nbsp;www.facebook.com/crigroups</div>
+<div className="i3"><i class="fas fa-globe"></i> &nbsp;www.crigroups.com</div>
+
+</div>
+        </div>
     </div>
+</div>
   );
 }
 
